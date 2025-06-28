24182be6a29008d945f8a00de7fbf63ea62515a2
yyy-anime <haygontt@gmail.com>
2025-06-28 14:56:20 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -14,7 +14,7 @@ const animeList = [
   {
     title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-153 ซับไทย",
     img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
-    type: "ตอนที่ 1-152 ซับไทย",
+    type: "ตอนที่ 1-153 ซับไทย",
     link: "Fights-Break-Sphere-Season-5"
   },
   {
