1aaa93fc39432b768038ba933302e1b53667d0d2
yyy-anime <haygontt@gmail.com>
2025-06-28 11:57:08 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -12,7 +12,7 @@ const animeList = [
     link: "black-clover-thai"
   },
   {
-    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-152 ซับไทย",
+    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-153 ซับไทย",
     img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
     type: "ตอนที่ 1-152 ซับไทย",
     link: "Fights-Break-Sphere-Season-5"
